55e069c79c61ce380bd5dcbc1257515001f74287
xXG470Xx <irviing1234@gmail.com>
2024-07-12 14:43:33 -0600

    Subida de archivo

---
+++ src/main/java/com/desadio_3/foroHub/ForoHubApplication.java
@@ -0,0 +1,13 @@
+package com.desadio_3.foroHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForoHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForoHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desadio_3/foroHub/ForoHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desadio_3.foroHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForoHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
